910b071e8287b9fdf45edcfb1b7520fee9b6f60e
alexpaden <padenalex0@gmail.com>
2023-08-18 16:01:38 -0500

    add gradient

--- packages/nextjs/components/example-ui/ContractData.tsx
+++ packages/nextjs/components/example-ui/ContractData.tsx
@@ -24,7 +24,7 @@ export const ContractData = () => {
   });
 
   return (
-    <div className="flex flex-col justify-center items-center max-w-[100vw]">
+    <div className="flex flex-col justify-center items-center bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] py-10 px-5 sm:px-0 lg:py-auto max-w-[100vw]">
       <div className="max-w-md bg-base-200 bg-opacity-70 rounded-2xl shadow-lg px-5 py-4 w-full">
         <div className="bg-secondary border border-primary rounded-xl flex">
           <div className="p-2 py-1 border-r border-primary flex items-end">Total Score</div>
