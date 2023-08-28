d3cd2558c4503d77072b67268c43042b1946f500
alex paden <padenalex0@gmail.com>
2023-08-28 01:15:56 -0500

    cleanup input

--- packages/nextjs/components/scaffold-eth/Input/InputBase.tsx
+++ packages/nextjs/components/scaffold-eth/Input/InputBase.tsx
@@ -32,10 +32,10 @@ export const InputBase = <T extends { toString: () => string } | undefined = str
   );
 
   return (
-    <div className={`flex border-2 border-base-300 bg-base-200 rounded-full text-accent ${modifier}`}>
+    <div className={`flex border-2 text-accent ${modifier}`}>
       {prefix}
       <input
-        className="input input-ghost focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
+        className="focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-auto font-medium placeholder:text-accent/50 text-gray-400 rounded-0 bg-white" // Added bg-white here
         placeholder={placeholder}
         name={name}
         value={value?.toString()}
